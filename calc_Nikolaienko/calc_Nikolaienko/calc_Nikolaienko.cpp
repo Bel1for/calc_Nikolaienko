abcbc8a411e79a3c3b5e901367e0ba1367c2e68c
student <student@05-10>
2021-12-09 20:50:14 +0200

    add div by zero aert in / funcs()

--- calc_Nikolaienko/calc_Nikolaienko/calc_Nikolaienko.cpp
+++ calc_Nikolaienko/calc_Nikolaienko/calc_Nikolaienko.cpp
@@ -1,4 +1,21 @@
 ﻿#include <iostream>
+#include <iostream>
+int funcPluc(int a, int b)
+{
+    return a + b;
+}
+int funcMinus(int a, int b)
+{
+    return a - b;
+}
+int funcUmn(int a, int b)
+{
+    return a * b;
+}
+int funcDel(int a, int b)
+{
+    return a / b;
+}
 int funcBol (int a, int b)
 {
     if (a > b)
@@ -9,8 +26,20 @@ int funcMen(int a, int b)
     if (a < b)
         return b;
 }
+int funcDiv(int a, int b)
+{
+    if (b > 0)
+        return a / b;
+    else
+        return 0;
+}
 int main()
 {
+    std::cout << funcPluc(18, 12)<<"\n";
+    std::cout << funcMinus(18, 12) << "\n";
+    std::cout << funcUmn(2, 4) << "\n";
+    std::cout << funcDel(4, 2) << "\n";
     std::cout << funcBol(2, 4)<<"\n";
     std::cout << funcMen(4, 2)<<"\n";
+    std::cout << funcDiv(4, 0) << "\n";
 }
